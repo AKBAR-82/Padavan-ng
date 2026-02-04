cb22753ddf572b8ccde3541a8fc02c1e64b32c0a
AKBAR-82 <148188410+AKBAR-82@users.noreply.github.com>
2026-02-04 14:55:20 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,1 @@
-# script is run before the build
-# e.g.:
-# sudo apt install <pkg>
+sed -i 's/^CONFIG_RT_SINGLE_SKU=y*/# CONFIG_RT_SINGLE_SKU is not set/' padavan-ng/trunk/configs/boards/ASUS/RT-N56UB1/kernel-3.4.x.config
